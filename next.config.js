ec86f91674e5d935f359208386ced3446d46fcf9
jessevandenberg <156073960+jessevandenberg@users.noreply.github.com>
2025-06-25 17:35:39 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -6,10 +6,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true
 });
 
 module.exports = nextConfig; 
